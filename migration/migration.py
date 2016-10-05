161bfeebd15ba9ca5325ba3845052db96b2a7ec6
ZoidQC <zoid@leetzone.ca>
2016-10-04 21:04:26 -0400

    Migration Eathing Others done

--- migration/migration.py
+++ migration/migration.py
@@ -44,7 +44,10 @@ for row in cursorV1.execute("""	SELECT LunchDescription AS Name FROM LH_Eatings 
 								UNION
 								SELECT SupperDescription AS Name FROM LH_Eatings WHERE SupperDescription <> '' """):
 	food_activity_query = "INSERT INTO activities(user_id, created_date, name, activity_type_id) VALUES(?, ?, ?, ?)"
-	print(food_activity_query)
+	cursorV2.execute(food_activity_query, (destination_user_id, datetime.now(), row[0], food_category_id))
+
+for row in cursorV1.execute("SELECT DISTINCT Description FROM LH_EatingOthers"):
+	food_activity_query = "INSERT INTO activities(user_id, created_date, name, activity_type_id) VALUES(?, ?, ?, ?)"
 	cursorV2.execute(food_activity_query, (destination_user_id, datetime.now(), row[0], food_category_id))
 
 #Create days
@@ -73,7 +76,22 @@ for row in cursorV1.execute("""	SELECT LunchDescription AS Name, LunchHour AS Ti
 
 	life_entry_id = cursorV2.lastrowid
 	
-	life_entry_activity_query = "INSERT INTO life_entry_activities(user_id, created_date, life_entry_id, activity_id, Quantity) VALUES(?, ?, ?, ?, ?)"
+	life_entry_activity_query = "INSERT INTO life_entry_activities(user_id, created_date, life_entry_id, activity_id, quantity) VALUES(?, ?, ?, ?, ?)"
+	cursorV2.execute(life_entry_activity_query, (destination_user_id, datetime.now(), life_entry_id, activity_id, row[3]))
+
+for row in cursorV1.execute("SELECT Description, Hour, Date, Comment FROM LH_EatingOthers"):
+	cursorV2.execute("SELECT id FROM activities WHERE name = ?", (row[0],))
+	activity_id = cursorV2.fetchone()[0]
+	
+	cursorV2.execute("SELECT id FROM days WHERE date = ?", (date_to_string(string_to_date(row[2])),))
+	day_id = cursorV2.fetchone()[0]
+
+	life_entry_query = "INSERT INTO life_entries(user_id, created_date, day_id, start_time) VALUES(?, ?, ?, ?)"
+	cursorV2.execute(life_entry_query, (destination_user_id, datetime.now(), day_id, get_correct_time_str(row[1])))
+
+	life_entry_id = cursorV2.lastrowid
+	
+	life_entry_activity_query = "INSERT INTO life_entry_activities(user_id, created_date, life_entry_id, activity_id, description) VALUES(?, ?, ?, ?, ?)"
 	cursorV2.execute(life_entry_activity_query, (destination_user_id, datetime.now(), life_entry_id, activity_id, row[3]))
 
 
