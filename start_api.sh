c09710896f4738a2bd7964695acbb34537c5374c
Kaven Thériault <zoid@leetzone.ca>
2017-07-26 19:21:43 -0400

    update start_api.sh

--- start_api.sh
+++ start_api.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-source /home/zoidqc/git/lifehistory.web/venv/bin/activate
-python /home/zoidqc/git/lifehistory.web/api.py
+source /home/zoidqc/git/lifehistory.api/venv/bin/activate
+python /home/zoidqc/git/lifehistory.api/api.py
